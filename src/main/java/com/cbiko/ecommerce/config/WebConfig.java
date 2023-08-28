13e44b14f4d945bdce9309f2a2ed234d74368838
Abdullah Salih Öner <64440843+AbdullahSalihOner@users.noreply.github.com>
2023-08-28 10:47:26 +0300

    28/08

--- src/main/java/com/cbiko/ecommerce/config/WebConfig.java
+++ src/main/java/com/cbiko/ecommerce/config/WebConfig.java
@@ -7,11 +7,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    //Tüm Durumlar için Cors Policy problemini çözüyoruz
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("*");
     }
+
+
+
 }
 
 
